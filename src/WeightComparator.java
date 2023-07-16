94eae90728be62ded7c5cdee17b9e3a2004dbcf4
Nileena John <94191050+nileenajohn@users.noreply.github.com>
2023-07-16 13:37:28 -0400

    Delete src directory

--- src/WeightComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-// represents a comparator that compares the costs of walls
-class WeightComparator implements Comparator<Wall> {
-  public int compare(Wall w1, Wall w2) {
-    return w1.cost - w2.cost;
-  }
-}
